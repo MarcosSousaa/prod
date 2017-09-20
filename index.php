84d9e6f78f988fe78b34375627d3f0eb84c15295
marcos.sousa <marcos.sousa@BAND-50.bandeirante.local>
2017-09-20 15:56:33 -0300

    new commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Controle de Produção | Ind Bandeirante de Plásticos</title>
+    </head>    
+    <body>
+        
+        <?php
+            
+        ?>
+    </body>
+</html>
