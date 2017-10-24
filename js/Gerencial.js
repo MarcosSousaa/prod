5a5d6bcdc79fd0cdea81515bc3af660486739699
marcos.sousa <marcos.sousa@BAND-50.bandeirante.local>
2017-10-24 15:17:36 -0200

    Acerto da aplicacao

--- js/Gerencial.js
+++ js/Gerencial.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
     $("#grafico_anual2").hide();
     $('#right-conteudo').hide();
     $('#right-conteudo2').hide();
+    $('#btn_voltar').hide();
     
     $(".estatic").click(function(){        
         $('#right-conteudo').show("slide");
@@ -55,80 +56,74 @@ $(document).ready(function(){
     });
     
       
-    $("#grafic").change(function(){
-        var grafico = $('option:selected',this).val();
+    //$("#grafic").change(function(){
+    $("#btn_gerar").click(function(){        
         var data1 = $('#data_1').val();        
         var newdata1 = data1.split("/").reverse().join("-");                        
         var data2 = $('#data_2').val();        
-        var newdata2 = data2.split("/").reverse().join("-");
-        
-        if(grafico == '00'){
-            $('#tabela1 tbody').empty();
-            $('#tabela2 tbody').empty();
-            obj = new Object();
-            obj.acao = 6;
-            obj.data1 = newdata1;
-            obj.data2 = newdata2;             
-            obj = JSON.stringify(obj);       
-            geraGrafico0(obj);        
-        }
-        else if(grafico == '01'){
-            $('#tabela3 tbody').empty();
-            $('#tabela4 tbody').empty();
-            obj = new Object();
-            obj.acao = 7;
-            obj.data1 = newdata1;
-            obj.data2 = newdata2;             
-            obj = JSON.stringify(obj);       
-            geraGrafico1(obj);        
-        }
-        else if(grafico == '02'){
-            $('#tabela5 tbody').empty();
-            $('#tabela6 tbody').empty();
-            obj = new Object();
-            obj.acao = 8;
-            obj.data1 = newdata1;
-            obj.data2 = newdata2;            
-            obj = JSON.stringify(obj);       
-            geraGrafico2(obj);       
-        }
-    });
+        var newdata2 = data2.split("/").reverse().join("-");                
+        $('#tabela1 tbody').empty();
+        $('#tabela2 tbody').empty();
+        obj = new Object();
+        obj.acao = 6;
+        obj.data1 = newdata1;
+        obj.data2 = newdata2;             
+        obj = JSON.stringify(obj);       
+        geraGrafico0(obj);        
+        $('#tabela3 tbody').empty();
+        $('#tabela4 tbody').empty();
+        obj = new Object();
+        obj.acao = 7;
+        obj.data1 = newdata1;
+        obj.data2 = newdata2;             
+        obj = JSON.stringify(obj);       
+        geraGrafico1(obj);                        
+        $('#tabela5 tbody').empty();
+        $('#tabela6 tbody').empty();
+        obj = new Object();
+        obj.acao = 8;
+        obj.data1 = newdata1;
+        obj.data2 = newdata2;            
+        obj = JSON.stringify(obj);       
+        geraGrafico2(obj);
+        $('#estatisticas').hide();
+    });        
     
-    
-    $("#grafic_anual").change(function(){
-        var grafico = $('option:selected',this).val();
+    //$("#grafic_anual").change(function(){
+    $('#btn_gerar2').click(function(){        
         var ano1 = $('#ano_1').val();                
-        var ano2 = $('#ano_2').val();                        
-        if(grafico == '00'){
-            $('#tabela7 tbody').empty();            
-            obj = new Object();
-            obj.acao = 9;
-            obj.ano1 = ano1;
-            obj.ano2 = ano2;             
-            obj = JSON.stringify(obj);       
-            geraGraficoAnual0(obj);        
-        }
-        else if(grafico == '01'){
-            $('#tabela9 tbody').empty();
-            $('#tabela10 tbody').empty();
-            obj = new Object();
-            obj.acao = 10;
-            obj.ano1 = ano1;
-            obj.ano2 = ano2;             
-            obj = JSON.stringify(obj);       
-            geraGraficoAnual1(obj);        
-        }
-        else if(grafico == '02'){
-            $('#tabela11 tbody').empty();
-            $('#tabela12 tbody').empty();
-            obj = new Object();
-            obj.acao = 11;           
-            obj.ano1 = ano1;
-            obj.ano2 = ano2;             
-            obj = JSON.stringify(obj);       
-            geraGraficoAnual2(obj);       
-        }
+        var ano2 = $('#ano_2').val();               
+        $('#tabela7 tbody').empty();             
+        $('#tabela8 tbody').empty();
+        obj = new Object();
+        obj.acao = 9;
+        obj.ano1 = ano1;
+        obj.ano2 = ano2;             
+        obj = JSON.stringify(obj);       
+        geraGraficoAnual0(obj);                        
+        /*       
+        $('#tabela9 tbody').empty();
+        $('#tabela10 tbody').empty();
+        obj = new Object();
+        obj.acao = 10;
+        obj.ano1 = ano1;
+        obj.ano2 = ano2;             
+        obj = JSON.stringify(obj);       
+        geraGraficoAnual1(obj);                        
+        $('#tabela11 tbody').empty();
+        $('#tabela12 tbody').empty();
+        obj = new Object();
+        obj.acao = 11;           
+        obj.ano1 = ano1;
+        obj.ano2 = ano2;             
+        obj = JSON.stringify(obj);       
+        geraGraficoAnual2(obj);       
+        */
+       $('#estatisticas2').hide();
     });
+    
+    
+    
     
     // GRAFICO GERAL
     function geraGrafico0(item){
@@ -138,10 +133,8 @@ $(document).ready(function(){
            url: "php/control/controller.php",
            dataType: 'json',
         }).done(function(result){
-            $("#grafico0").show();
-            $("#grafico1").hide();
-            $("#grafico2").hide();
-            var producao = [];
+            $("#grafico0").show();            
+                var producao = [];
                 var apara = [];
                 var refile = [];
                 var borra = [];
@@ -211,9 +204,7 @@ $(document).ready(function(){
             url: "php/control/controller.php",
             dataType: 'json',
             }).done(function(result2){                     
-                $("#grafico1").show();
-                $("#grafico2").hide();
-                $("#grafico0").hide();
+                $("#grafico1").show();                
                 var producao2 = [];
                 var apara2 = [];
                 var refile2 = [];
@@ -302,9 +293,7 @@ $(document).ready(function(){
             url: "php/control/controller.php",
             dataType: 'json',
         }).done(function(result){                
-            $("#grafico2").show();
-            $("#grafico1").hide();
-            $("#grafico0").hide();
+            $("#grafico2").show();            
             var extrusora = [];
             var producao = [];
             var apara = [];
@@ -403,78 +392,118 @@ $(document).ready(function(){
            dataType: 'json',
         }).done(function(result){                               
             $("#grafico_anual0").show();
-            $("#grafico_anual1").hide();
-            $("#grafico_anual2").hide();
-            var mes = [];
-            var ano = [];
-            var producao = [];            
-            var perda = [];                            
-            var cor1 = ['rgba(41, 128, 185,1.0)', 'rgba(41, 128, 185,1.0)', 'rgba(41, 128, 185,1.0)','rgba(41, 128, 185,1.0)', 'rgba(41, 128, 185,1.0)', 'rgba(41, 128, 185,1.0)','rgba(41, 128, 185,1.0)', 'rgba(41, 128, 185,1.0)', 'rgba(41, 128, 185,1.0)'];
-            var cor2 = ['rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)'];
-            var cor3 = ['rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)'];
-            var cor4 = ['rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)',];                       
-            for(var i in result){                    
-                producao.push(result[i].PRODUCAO);                                                                                
-                perda.push(result[i].PERDA);
-                ano.push(result[i].ANO);
-                mes.push(result[i].NOME_MES);              
-                var soma1 = parseFloat(result[i].PRODUCAO) + parseFloat(result[i].PERDA);
-                var percentual1 = (parseFloat(result[i].PERDA) * 100) / soma1;                
-                $("#tabela7 tbody").append("<tr><td>"+result[i].ANO+"</td><td>"+result[i].NOME_MES+"</td><td>"+result[i].PRODUCAO+ "</td><td>"+result[i].PERDA+"</td><td style='color:blue;'>"+soma1.toFixed(3)+"</td><td style='color: red;'><strong>"+percentual1.toFixed(1)+"%</strong></td>");                                                                
-                
-            }            
-                $(".comparar").append("<a class='waves-effect waves-light btn btn-adc'>one</a>")
-            function remove_duplicates_safe(arr) {
-                var seen = {};
-                var ret_arr = [];
-                for (var i = 0; i < arr.length; i++) {
-                    if (!(arr[i] in seen)) {
-                        ret_arr.push(arr[i]);
-                        seen[arr[i]] = true;
-                    }
-                }
-                return ret_arr;
-            }
-             var ano2 = remove_duplicates_safe(ano);
+            $(".foto-voltar").show();
             var geralAnualData = {
-                labels: mes,
+                labels: ["Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"],
+                
                 datasets: [
-                    {
-                        label: "Producao "+ano2,
-                        data: producao,
-                        backgroundColor: cor1,
-                    },
-                    {
-                        label: "Perda "+ano2,
-                        data: perda,
-                        backgroundColor: cor2,
-                    }
+                                        
                     
                 ]
-                };
-            var anu0 = $("#mycanvasanual0");  
+                
+            }
+            
+            var anu0 = $("#mycanvasanual0");                
             var barAnual = new Chart(anu0, {
                 type: 'bar',
                 data: geralAnualData
-            });
-         $('.comparar').delegate(".btn-adc","click",function(){
-    var newDataset = {        
-        label: "Grafico 2",
-        backgroundColor: 'rgba(99, 255, 132, 0.2)',
-        borderColor: 'rgba(99, 255, 132, 1)',
-        borderWidth: 1,
-        data: [60300, 200500],
-    };
-    var newDataset2 = {        
-        label: "Grafico 2",
-        backgroundColor: 'rgba(100, 155, 132, 0.2)',        
-        borderWidth: 1,
-        data: [65300, 200800],
-    };
-    geralAnualData.datasets.push(newDataset);
-    geralAnualData.datasets.push(newDataset2);
-    barAnual.update();
-    });
+            });                        
+            var total_produzido = [];
+            var total_perda = [];
+            var ano = [];                                    
+            var jan = [];
+            var fev = [];
+            var mar = [];
+            var abr = [];
+            var mai = [];
+            var jun = [];
+            var jul = [];
+            var ago = [];
+            var set = [];
+            var out = [];
+            var nov = [];
+            var dez = [];
+            var perda_jan = [];
+            var perda_fev = [];
+            var perda_mar = [];
+            var perda_abr = [];
+            var perda_mai = [];
+            var perda_jun = [];
+            var perda_jul = [];
+            var perda_ago = [];
+            var perda_set = [];
+            var perda_out = [];
+            var perda_nov = [];
+            var perda_dez = [];
+            var cor = [];
+            cor[0] = ['rgba(41, 128, 185,1.0)', 'rgba(41, 128, 185,1.0)', 'rgba(41, 128, 185,1.0)','rgba(41, 128, 185,1.0)', 'rgba(41, 128, 185,1.0)', 'rgba(41, 128, 185,1.0)','rgba(41, 128, 185,1.0)', 'rgba(41, 128, 185,1.0)', 'rgba(41, 128, 185,1.0)','rgba(41, 128, 185,1.0)', 'rgba(41, 128, 185,1.0)', 'rgba(41, 128, 185,1.0)'];
+            cor[1] = ['rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)','rgba(192, 57, 43,1.0)'];
+            cor[2] = ['rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)','rgba(44, 62, 80,1.0)'];
+            cor[3] = ['rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)','rgba(39, 174, 96,1.0)'];                       
+            cor[4] = ['rgba(155, 89, 182,1.0)','rgba(155, 89, 182,1.0)','rgba(155, 89, 182,1.0)','rgba(155, 89, 182,1.0)','rgba(155, 89, 182,1.0)','rgba(155, 89, 182,1.0)','rgba(155, 89, 182,1.0)','rgba(155, 89, 182,1.0)','rgba(155, 89, 182,1.0)','rgba(155, 89, 182,1.0)','rgba(155, 89, 182,1.0)','rgba(155, 89, 182,1.0)'];
+            cor[5] = ['rgba(22, 160, 133,1.0)','rgba(22, 160, 133,1.0)','rgba(22, 160, 133,1.0)','rgba(22, 160, 133,1.0)','rgba(22, 160, 133,1.0)','rgba(22, 160, 133,1.0)','rgba(22, 160, 133,1.0)','rgba(22, 160, 133,1.0)','rgba(22, 160, 133,1.0)','rgba(22, 160, 133,1.0)','rgba(22, 160, 133,1.0)','rgba(22, 160, 133,1.0)',];
+            var x = 0;
+            var z = 0;
+            for(var i in result){                
+                while(x <= i){                
+                    ano = result[i].ano;                    
+                    jan = result[i].janeiro;
+                    fev = result[i].fevereiro;
+                    mar = result[i].marco;
+                    abr = result[i].abril;
+                    mai = result[i].maio;
+                    jun = result[i].junho;
+                    jul = result[i].julho;
+                    ago = result[i].agosto;
+                    set = result[i].setembro;
+                    out = result[i].outubro;
+                    nov = result[i].novembro;
+                    dez = result[i].dezembro;
+                    perda_jan = result[i].perda_janeiro;
+                    perda_fev = result[i].perda_fevereiro;
+                    perda_mar = result[i].perda_marco;
+                    perda_abr = result[i].perda_abril;
+                    perda_mai = result[i].perda_maio;
+                    perda_jun = result[i].perda_junho;
+                    perda_jul = result[i].perda_julho;
+                    perda_ago = result[i].perda_agosto;
+                    perda_set = result[i].perda_setembro;
+                    perda_out = result[i].perda_outubro;
+                    perda_nov = result[i].perda_novembro;
+                    perda_dez = result[i].perda_dezembro;                                          
+                    var dataset = {
+                        label: "Producao "+ ano,
+                        backgroundColor: cor[z],
+                        data:[jan,fev,mar,abr,mai,jun,jul,ago,set,out,nov,dez],
+                       
+                    };
+                    var cor2 = z + 1;
+                    var dataset2 = {
+                        label: "Perda "+ ano,
+                        backgroundColor: cor[cor2],
+                        data:[perda_jan,perda_fev,perda_mar,perda_abr,perda_mai,perda_jun,perda_jul,perda_ago,perda_set,perda_out,perda_nov,perda_dez],
+                       
+                    };
+                    geralAnualData.datasets.push(dataset);
+                    geralAnualData.datasets.push(dataset2);
+                    barAnual.update();
+                    x = x + 1;
+                    z = z + 2;
+                    $("#tabela7 tbody").append("<tr><td>Producao</td><td>"+ano+"</td><td>"+jan+"</td><td>"+fev+"</td><td>"+mar+ "</td><td>"+abr+"</td><td>"+mai+"</td><td>"+jun+"</td><td>"+jul+"</td><td>"+ago+ "</td><td>"+set+"</td><td>"+out+"</td><td>"+nov+"</td><td>"+dez+"</td><td></td><td style='color:blue;'>"+result[i].total_produzido+"</td>");
+                    $("#tabela8 tbody").append("<tr><td>Perda</td><td>"+ano+"</td><td>"+perda_jan+"</td><td>"+perda_fev+"</td><td>"+perda_mar+ "</td><td>"+perda_abr+"</td><td>"+perda_mai+"</td><td>"+perda_jun+"</td><td>"+perda_jul+"</td><td>"+perda_ago+ "</td><td>"+perda_set+"</td><td>"+perda_out+"</td><td>"+perda_nov+"</td><td>"+perda_dez+"</td><td></td><td style='color:blue;'>"+result[i].total_perda+"</td>");
+                }
+                
+            }            
+
+            
+            //geralAnualData.datasets.push(newDataset1);            
+            //barAnual.update();
+            
+            
+            
+            
+            //$(".comparar").append("<a class='waves-effect waves-light btn btn-adc'>one</a>")
+            
         }).fail(function(result){
             alert('NÃO RODOU');
         });
